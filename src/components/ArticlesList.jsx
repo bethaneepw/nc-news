632c0f9d2e9002a685acea2ab2d8d33a96f6a3c6
Bethany White <bethanypatriciaw@gmail.com>
2025-05-20 15:45:38 +0100

    removing redundant code

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -35,7 +35,7 @@ return (
     <section>
         <label htmlFor="limit">Viewing: </label>
         <select name="limit" defaultValue="10" onChange={handleLimit}>
-            <option value="5" onSelect={()=>(5)}>5</option>
+            <option value="5">5</option>
             <option value="10">10</option>
             <option value="20">20</option>
             <option value="50">50</option>
