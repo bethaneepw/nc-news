6491db30895bc95764432117a3a4c04193e94e0e
Bethany White <bethanypatriciaw@gmail.com>
2025-05-20 16:59:26 +0100

    add base functionality for viewing individual articles

--- src/components/ArticleInfoCard.jsx
+++ src/components/ArticleInfoCard.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
 import { formatArticleInfo } from "../../utils/utils";
 
 function ArticleInfoCard ({articleToDisplay}) {
 
     const formattedArticle = formatArticleInfo(articleToDisplay)
-
     return (
         <>
             <p className="info-card-title">{formattedArticle.title}</p>
             <p>{formattedArticle.topic}</p>
             <p>Posted: {formattedArticle.created_at}<br/>{formattedArticle.comment_count} comments <br/>{formattedArticle.votes} votes</p>
             <p>{formattedArticle.author}</p>
+            <Link to={`/articles/${formattedArticle.article_id}`}><button>View</button></Link>
+            
         </>
         
     )
