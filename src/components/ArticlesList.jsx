eae129cb624ccb92f60b0e22139075b03cdd971c
Bethany White <bethanypatriciaw@gmail.com>
2025-05-22 16:37:47 +0100

    add sorting articles functionality

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -10,15 +10,12 @@ const [page, setPage] = useState(1)
 const [limit, setLimit] = useState(10)
 const [totalCount, setTotalCount] = useState(null)
 const [searchParams, setSearchParams] = useSearchParams();
-
+const [sort, setSort] = useState("created_at")
+const [order, setOrder] = useState("DESC")
 useEffect(()=>{
     setIsLoading(true)
-
-    // const sort_by = searchParams.get('sort')
-    // const order = searchParams.get('order')
     const topic = searchParams.get('topic')
-   
-    getArticles({page, limit, topic})
+    getArticles({page, limit, topic, sort, order})
     .then(({articles, total_count})=>{
         setArticlesToList(articles)
         setTotalCount(total_count)
@@ -35,6 +32,22 @@ function handleLimit(event) {
 setLimit(event.target.value)
 }
 
+function handleSort(event) {
+setSort(event.target.value)
+}
+
+function handleOrder(event) {
+setOrder(event.target.value)
+}
+
+function handleFilter () {
+        setSearchParams({
+            ...Object.fromEntries(searchParams),
+            sort,
+            order
+        });
+    };
+
 return (
     <>
     <div className="topic-button-group">
@@ -42,12 +55,26 @@ return (
     <Link to="/articles?topic=coding"><button>Coding</button></Link>
     <Link to="/articles?topic=football"><button>Football</button></Link>
     </div>
+
+    <div className="sort-button-group">
+        <select name="sort" id="sort" value={sort} onChange={handleSort}>
+            <option value="created_at">Date posted</option>
+            <option value="votes">Votes</option>
+            <option value="author">Author</option>
+        </select>
+         <select name="Order" id="Order" value={order} onChange={handleOrder}>
+            <option value="ASC">Ascending</option>
+            <option value="DESC">Descending</option>
+        </select>
+            <button onClick={handleFilter}>Filter</button>
+        
+    </div>
    
     {isLoading ? <p> Loading Articles...</p> : 
     
     <section>
         <label htmlFor="limit">Viewing: </label>
-        <select name="limit" defaultValue="10" onChange={handleLimit}>
+        <select name="limit" defaultValue={limit} onChange={handleLimit}>
             <option value="5">5</option>
             <option value="10">10</option>
             <option value="20">20</option>
